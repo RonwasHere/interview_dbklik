4cb54116e28fca8c86dcfbcdc3bc7cd221f7158c
RonwasHere <ronaldliansugihartono@gmail.com>
2023-07-03 17:10:41 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/interview_dbklik/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.interview_dbklik
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ luar/satu.dart
@@ -0,0 +1,16 @@
+void main(List<String> args) {
+  List<int> listInt = [];
+
+  listInt.add(1);
+  listInt.add(2);
+  listInt.add(3);
+
+  print(listInt);
+
+  var names = <String>[];
+  names.add('budi');
+  names.add('joko');
+  names.add('tono');
+
+  print(names);
+}
